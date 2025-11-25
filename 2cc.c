c620dfd79df6b7878832bc29b6bcde378c87527b
Yangm8i <ryo.wata2@gmail.com>
2025-11-25 17:07:08 +0900

    バグの修正

--- 2cc.c
+++ 2cc.c
@@ -46,61 +46,6 @@ Token *token;
 // 入力プログラム
 char *user_input;
 
-// 2項演算子のノードを作成
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
-    Node *node = calloc(1, sizeof(Node));
-    node->kind = kind;
-    node->lhs = lhs;
-    node->rhs = rhs;
-    return node;
-}
-
-// 数値のノードを作成
-Node *new_node_num(int val) {
-    Node *node = calloc(1, sizeof(Node));
-    node->kind = ND_NUM;
-    node->val = val;
-    return node;
-}
-
-Node *primary() {
-    // 次のトークンが"("なら、"(" expr ")" のはず
-    if(consume('(')) {
-        Node *node = expr();
-        expect(')');
-        return node;
-    }
-
-    // そうでなければ数値のはず
-    return new_node_num(expect_number());
-}
-
-Node *mul() {
-    Node *node = primary();
-
-    for(;;) {
-        if(consume('*'))
-            node = new_node(ND_MUL, node, primary());
-        else if(consume('/'))
-            node = new_node(ND_DIV, node, primary());
-        else
-            return node;
-    }
-}
-
-Node *expr() {
-    Node *node = mul();
-
-    for(;;) {
-        if (consume('+'))
-            node = new_node(ND_ADD, node, mul());
-        else if (consume('-'))
-            node = new_node(ND_SUB, node, mul());
-        else
-            return node;
-    }
-}
-
 // エラー箇所を報告する
 void error_at(char *loc, char *fmt, ...){
     va_list ap;
